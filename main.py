4039660c317f39a18a8f20e74da2b7fd9077fb91
BrooksOBrien22 <824084@mystma.org>
2021-09-15 14:44:11 -0500

    added top three cheeses

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-print("Hello World!")
-print("I like chicken nuggets")
+#define topFiveFoods
+def topFiveFoods():
+    #print first food
+    print("Orange Chicken")   
 
-#Print first cheese
-print("swiss")
+    #print second food
+    print("Wings")
 
-#print second cheese
-print("pepper jack")
+    #print third food
+    print("Burgers")
 
-#print third cheese
-print("cheddar")
+    #print fourth food
+    print("Noodles")
+
+    #print fifth food
+    print("steak")
+def topThreeSports():
+    print("Football")
+    print("Basketball")
+    print("Trapshooting")
+def topThreeDays():
+    print("Friday")
+    print("Saturday")
+    print("Sunday")
+def topThreeClasses():
+    print("Programming")
+    print("Gym")
+    print("Math")
+def topThreeMeals():
+    print("Dinner")
+    print("Lunch")
+    print("Breakfast")
+topFiveFoods()
+topThreeClasses()
+topThreeDays()
+topThreeMeals()
+topThreeSports()
