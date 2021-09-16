4e3e8d4346da4bdc74be0042f6c7b2e830e1792c
BrooksOBrien22 <824084@mystma.org>
2021-09-16 15:15:52 -0500

    added madlib

--- main.py
+++ main.py
@@ -35,3 +35,20 @@ def myNameIs(banana):
 
 myNameIs("Brooks")
 myNameIs("Logan")
+
+def timesTen(num):
+    print(num * 10)
+
+def plusFive(plus):
+    print(plus + 5)
+
+def divideByTwo(div):
+    print(div / 2)
+
+def addTwoNumbas(x,y):
+    print(x + y)
+
+def madLib(instrument, hours, day, letter, majomin, flats, sharps, adjectone, adjecttwo, fract, adjectthree, instrumenttwo, adjectfour, noun, family, song, adjectfive, artist):
+    print("While I've stayed at home, I've practiced the" + instrument + " for" + hours + " hours every" + day + ". My favorite key signature to play and practice in is" + letter + majomin + ". It has" + flats + sharps + ". I like this key signature because it is" + adjectone + " and" + adjecttwo + ". I've also gotten awesome at counting time signatures. The time signature I love is" + fract + " because it is" + adjectthree + " to count. One instrument I want to learn how to play while I'm at home is" + instrumenttwo + ", because it is" + adjectfour + " and sounds like a" + noun + ". My" + family + " likes it when I play" + song + ", and always gives me a round of applause after my performances! When I return to school, my teacher will be" + adjectfive + " of how great I am at playing my insturment. My teacher might even think I sound like" + artist + "!")
+
+madLib( "Trombone", "hours", "day", "letter", "majomin", "flats", "sharps", "adjectone", "adjecttwo", "fract", "adjectthree", "instrumenttwo", "adjectfour", "noun", "family", "song", "adjectfive", "artist")
